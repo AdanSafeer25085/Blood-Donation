d8cb9bc4adc71af3e22f4e3316ec11c58a758eea
AdanSafeer25085 <akraj25085@gmail.com>
2025-08-12 23:09:44 +0500

    nodmodules deleted

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const winston = require("winston");
 const jwt = require("jsonwebtoken");
 const supabase = require("./config/supabase");
