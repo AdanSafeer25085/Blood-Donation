afb0edaf64ac1f497a124991244a285d871830be
AdanSafeer25085 <akraj25085@gmail.com>
2025-08-12 22:53:22 +0500

    frontened changes

--- my-app/src/config/api.js
+++ my-app/src/config/api.js
@@ -1,4 +1,4 @@
 // API Configuration
-const API_BASE_URL = 'https://blood-donation-production-bd12.up.railway.app';
+const API_BASE_URL = 'https://blood-donation-production-e3ba.up.railway.app';
 
 export default API_BASE_URL;
